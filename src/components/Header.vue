265520366b69266362c0438d6e36c963a5f6d27f
GiovanniConti <giovanni.conti@live.it>
2021-11-15 16:29:57 +0100

    Header complete

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <img src="@/assets/spotify_logo.png" alt="Spotify_logo">
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+}
+</script>
+
+<style lang="scss">
+
+header{
+  background-color: #374753;
+  img{
+    width: 100px;
+  }
+}
+
+</style>
