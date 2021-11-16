c6f7fa743be598a064a5c808c1bc5fb1e751308e
GiovanniConti <giovanni.conti@live.it>
2021-11-16 17:20:18 +0100

    added "select" to select genre to filter

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="cardsCotainer container-lg my-5">
+    <div class="row justify-content-center pb-5">
+      <div class="col-6">
+        <FiltersSection
+        :availableGenres="genreList"
+        ></FiltersSection>
+      </div>
+    </div>
     <div class="row row-cols-5 g-4">
       <div class="col d-flex"
         v-for="(disc, i) in discList"
@@ -17,25 +24,40 @@
 </template>
 
 <script>
-import DiscCard from './DiscCard.vue';
 import axios from 'axios';
+import DiscCard from './DiscCard.vue';
+import FiltersSection from './FiltersSection.vue';
 
 export default {
   name: "CardsContianer",
   components: {
     DiscCard,
+    FiltersSection,
   },
   data() {
     return {
       discList: [],
     };
   },
+  computed: {
+    genreList() {
+      const genreList = [];
+
+      this.discList.forEach((disc) => {
+        if(!genreList.includes(disc.genre)){
+          genreList.push(disc.genre);
+        }
+      });
+
+      return genreList;
+    }
+  },
   methods: {
     fetchData(url) {
       axios.get(url).then((apiResp) => {
         this.discList = apiResp.data.response;
       });
-    }
+    },
   },
   mounted() {
     this.fetchData("https://flynn.boolean.careers/exercises/api/array/music");
---
+++ src/components/FiltersSection.vue
@@ -0,0 +1,26 @@
+<template>
+  <select class="form-select" aria-label="Default select example" v-model="selectedGenre">
+    <option
+      v-for="(genre, i) in availableGenres"
+      :key="i"
+      :value="genre">{{ genre }}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "FiltersSection",
+  props: {
+    availableGenres: Array
+  },
+  data() {
+    return {
+      selectedGenre: "",
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
